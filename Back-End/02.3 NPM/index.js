fde2c995e191404431a2f6cf670869c1004b1218
ayannareda <ayannareda09@gmail.com>
2025-10-09 20:18:17 +0530

    How to use npm

---
+++ Back-End/02.3 NPM/index.js
@@ -0,0 +1,10 @@
+import generateName from "sillyname";
+var myName = generateName();
+
+console.log("My name is " + myName);
+
+import {randomSuperhero} from "superheroes";
+
+var myHero = randomSuperhero();
+
+console.log("My hero is " + myHero);
